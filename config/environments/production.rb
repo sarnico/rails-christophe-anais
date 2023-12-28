da8e3f30e691e9cea439f88e9dd13e30a2750b09
sarnico <nicolay.sara@gmail.com>
2023-12-28 22:02:19 +0000

    ss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
+  
+  multi_db = false
   # Settings specified here will take precedence over those in config/application.rb.
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
